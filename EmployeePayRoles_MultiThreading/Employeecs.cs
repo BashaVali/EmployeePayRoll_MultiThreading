d75984a0d48e86e94980100403c0d365f15d861d
BashaVali <bashavali222@gmail.com>
2023-01-21 05:04:50 +0530

    AddEmployee_Without Thread

---
+++ EmployeePayRoles_MultiThreading/Employeecs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRoles_MultiThreading
+{
+    public class Employeecs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Salary { get; set; }
+        public string Gender { get; set; }
+        public Employeecs(int id, string name, long salary, string gender)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            Gender = gender;
+        }
+    }
+}
+
---
+++ EmployeePayRoles_MultiThreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayRole_MuktiThreading
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Employee pay role program running using multi threading ");
+        }
+    }
+}
